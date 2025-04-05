2a0ac35e57aaaf6e6c099f5f3c7be6564d395561
Nikookar <hmdnikookar@gmail.com>
2025-04-04 21:38:30 -0700

    Finalize MATCHAGG

--- main_shield.m
+++ main_shield.m
@@ -1,3 +1,4 @@
+% ---------------- MAIN SCRIPT ---------------- %
 clc
 clear
 clf('reset')
@@ -5,120 +6,80 @@ close all
 warning('off')
 
 %% initialize the script %%
-
-% address of second-stage langevin dynamics data to be imported
 fdir_in = 'F:\DLCA2\outputs\postLD2-01-Apr-2025_11-54-39_LD2-25NOV24';
 fname_in = 'Post_LD2-25NOV24';
 
-% variables of interest in the data
 varnames = {'parsdata'};
-
-% load second-stage LD aggregates
 for i = 1 : numel(varnames)
-    load(strcat(fdir_in, '\', fname_in, '.mat'), varnames{i})
+    load(fullfile(fdir_in, strcat(fname_in, '.mat')), varnames{i});
 end
 
-% assign a subset of aggregates for plotting
+ii0 = [1, 2, 3];
+n_agg_plt = length(ii0);
 
-ii0 = [1, 3, 5]; % determine post-flame snapshots from which the...
-    % ...aggregates to be plotted are chosen
+triad_pos = {[0.07, 0.71, 0.08, 0.08], [0.49, 0.71, 0.08, 0.08];
+             [0.07, 0.42, 0.08, 0.08], [0.49, 0.42, 0.08, 0.08];
+             [0.07, 0.11, 0.08, 0.08], [0.49, 0.11, 0.08, 0.08]};
+y_row_ttl = [0.96, 0.66, 0.37];
+x_row_ttl = [0.2, 0.6];
 
-% set target values on number of primary particles and polydispersity...
-    % ...to choose aggregates being plotted
-nPP_target = 110;
-sigmaPP_target = 1.3;
+% call MATCHAGG with fixed seed to ensure reproducibility and cap on npp
+jj0 = UTILS.MATCHAGG(vertcat(parsdata(ii0)), 'npp', 'sigmapp', 'n_hyb', ...
+    0.8, 80, 50.0, 10000, 38, 300);  % x_max_raw = 300
 
-% fixed triad locations (one per tile)
-triad_pos = {[0.08, 0.9, 0.08, 0.08], [0.85, 0.9, 0.08, 0.08];
-    [0.08, 0.43, 0.08, 0.08], [0.55, 0.43, 0.08, 0.08];
-    [0.08, 0.3, 0.08, 0.08], [0.85, 0.3, 0.08, 0.08]};
-    
-%% find aggregates to be rendered %%
-
-n_agg_plt = length(ii0); % numebr of hybridity snapshots being rendered
-
-% normalized difference in number of primary particles between...
-    % ...repository and target
-dnPP_hat = cell(n_agg_plt,1);
-
-% normalized difference in polydispersity
-dsigmaPP_hat = cell(n_agg_plt,1);
-
-% indices of aggregates being rendered
-jj0 = zeros(n_agg_plt,1);
-
-% calculate normalized differences
-for i = 1 : n_agg_plt
-
-    dnPP_hat{i} = abs(parsdata(ii0(i)).npp - nPP_target); 
-    dnPP_hat{i} = dnPP_hat{i} / max(dnPP_hat{i});
-
-    dsigmaPP_hat{i} = abs(parsdata(ii0(i)).sigmapp - sigmaPP_target); 
-    dsigmaPP_hat{i} = dsigmaPP_hat{i} / max(dsigmaPP_hat{i});
-    
-    % find aggregate that is closet to target (via minimizing differences)
-    jj0(i) = find((dnPP_hat{i} + dsigmaPP_hat{i}) ==...
-        min(dnPP_hat{i} + dsigmaPP_hat{i}), 1);
-
+% display selected properties
+for i = 1:n_agg_plt
+    fprintf('Group %d: npp = %d, sigmapp = %.4f, n_hyb = %d\n', i, ...
+        parsdata(ii0(i)).npp(jj0(i)), ...
+        parsdata(ii0(i)).sigmapp(jj0(i)), ...
+        parsdata(ii0(i)).n_hyb(jj0(i)));
 end
 
-%% plot subselected aggregates %%
-
-% initialize temporal dpp vs da figure
+% initialize figure
 f2 = figure(2);
 f2.Position = [100, 100, 600, 900];
 set(f2, 'color', 'white')
-
-% initialize layout
 tl2_tot = tiledlayout(n_agg_plt,2);
 tl2_tot.TileSpacing = 'compact';
 tl2_tot.Padding = 'compact';
 
-% define rendering color
 opts2.cc = 'on';
 opts2.cm = colormap("abyss");
 opts2.cm = flip(opts2.cm,1);
 
-tl2 = cell(n_agg_plt, 2); % placeholder for individual tiles
+tl2 = cell(n_agg_plt, 2);
+row_ttl = cell(3,1);
 
 for i = 1 : n_agg_plt
-
-    % first row: a nonhybrid aggregate
-    % other rows: aggregates with increasing hybridity (as moving down...
-        % ...the rows) but with the same number of primary particles and...
-        % ...polydispersity
-
-    tl2{i,1} = nexttile(tl2_tot, 2*i-1); % isometric view
-    
-    UTILS.PLOTPP(parsdata(ii0(1)).pp{jj0(i)}(:,3), parsdata(ii0(1)).pp{jj0(i)}(:,4),...
-        parsdata(ii0(1)).pp{jj0(i)}(:,5), parsdata(ii0(1)).pp{jj0(i)}(:,2),...
-        parsdata(ii0(1)).pp{jj0(i)}(:,2), opts2); % plot aggregate
+    tl2{i,1} = nexttile(tl2_tot, 2*i-1);
+    UTILS.PLOTPP(parsdata(ii0(i)).pp{jj0(i)}(:,3), parsdata(ii0(i)).pp{jj0(i)}(:,4), ...
+                 parsdata(ii0(i)).pp{jj0(i)}(:,5), parsdata(ii0(i)).pp{jj0(i)}(:,2), ...
+                 parsdata(ii0(i)).pp{jj0(i)}(:,2), opts2);
     hold on
-    
-    UTILS.MAKEAX(f2, triad_pos{i,1}, '3d'); % render x-y-z axes
-    
-    tl2{i,2} = nexttile(tl2_tot, 2*i); % x-y view
-    
-    UTILS.PLOTPP(parsdata(ii0(1)).pp{jj0(i)}(:,3), parsdata(ii0(1)).pp{jj0(i)}(:,4),...
-        parsdata(ii0(1)).pp{jj0(i)}(:,5), parsdata(ii0(1)).pp{jj0(i)}(:,2),...
-        parsdata(ii0(1)).pp{jj0(i)}(:,2), opts2); % plot aggregate
+    UTILS.MAKEAX(f2, triad_pos{i,1}, '3d');
+
+    tl2{i,2} = nexttile(tl2_tot, 2*i);
+    UTILS.PLOTPP(parsdata(ii0(i)).pp{jj0(i)}(:,3), parsdata(ii0(i)).pp{jj0(i)}(:,4), ...
+                 parsdata(ii0(i)).pp{jj0(i)}(:,5), parsdata(ii0(i)).pp{jj0(i)}(:,2), ...
+                 parsdata(ii0(i)).pp{jj0(i)}(:,2), opts2);
     hold on
     view(2)
+    UTILS.MAKEAX(f2, triad_pos{i,2}, 'xy');
 
-    UTILS.MAKEAX(f2, triad_pos{i,2}, 'xy'); % render x-y-z axes
+    row_ttl{i} = strcat('$n_\mathrm{pp}$ =', {' '}, num2str(parsdata(ii0(i)).npp(jj0(i))), ...
+        ', $\sigma_\mathrm{pp}$ =', {' '}, num2str(parsdata(ii0(i)).sigmapp(jj0(i)), '%.2f'), ...
+        ', $n_\mathrm{hyb}$ =', {' '}, num2str(parsdata(ii0(i)).n_hyb(jj0(i))));
 
+    annotation('textbox', [x_row_ttl(1), y_row_ttl(i), x_row_ttl(2), 0.03], ...
+        'String', row_ttl{i}, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom', ...
+        'FontSize', 14, 'EdgeColor', 'none', 'Interpreter', 'latex');
 end
 
-% add a shared colorbar under the whole layout
-cb2 = colorbar(tl2{1,1}, 'eastoutside');  % generate colorbar in first tile
-cb2.Layout.Tile = 'south'; % move it to the bottom
-cb2.Label.String = 'Shielding ratio [-]'; % add label
-cb2.FontSize = 12; % tick label font size
-cb2.Label.FontSize = 18; % label font size
-cb2.TickLabelInterpreter = 'latex'; % tick label format
-cb2.Label.Interpreter = 'latex'; % label format
-cb2.LineWidth = 1; % width of edges and ticks
-
-
-
-    
+cb2 = colorbar(tl2{1,1}, 'eastoutside');
+cb2.Layout.Tile = 'south';
+cb2.Label.String = 'Shielding ratio [-]';
+cb2.FontSize = 12;
+cb2.Label.FontSize = 18;
+cb2.TickLabelInterpreter = 'latex';
+cb2.Label.Interpreter = 'latex';
+cb2.LineWidth = 1;
