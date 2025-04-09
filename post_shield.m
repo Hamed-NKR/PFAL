472e905f6019420f78fbdcf93c6806381e476667
Hamed Nikookar <70616948+Hamed-NKR@users.noreply.github.com>
2025-04-09 00:16:08 -0700

    Update post-shield
    Boxplot showing bias in 2d projections of primary particle size added

--- post_shield.m
+++ post_shield.m
@@ -8,7 +8,7 @@ warning('off')
 
 % location of previously saved aggregate data (this should include...
     % ...spp field in pars strcuture for primary particle shielding)
-fdir_in = 'C:\Users\hmdnkr\Documents\GitHub\MCEM\outputs\Shield_07-Apr-2025_13-01-18';
+fdir_in = 'D:\Users\hmdni\OneDrive\Documents\GitHub\MCEM\outputs\Shield_07-Apr-2025_13-01-18';
 fname_in = 'Shield_07-Apr-2025_13-01-18';
 
 varnames = {'parsdata'}; % varaiables to be imported
@@ -28,6 +28,10 @@ y0_triad1 = [0.13, 0.98];
     % ...information)
 x_ttl1 = [0.2, 0.6];
 y0_ttl1 = [0.06, 0.96];
+
+% criteria on shielding ratio above which primary particles are...
+    % ...not accounted when looking at projections of aggregates
+spp_star = 0.5;
 
 %% render selected aggregates and colorcode their primary particles...
     % ...based on shielding factor %%
@@ -151,6 +155,7 @@ xlbl2 = cell(n_shot, 1);
 npp_tot = zeros(n_shot, 1); % allocate variable for the ensemble number...
     % ...of primary particles
 
+hold on
 for i = 1 : n_shot
 
     % concatinate shielding factors across aggregates
@@ -169,7 +174,6 @@ for i = 1 : n_shot
     end
     
     scatter(i-0.5, 1.2) % this is only to assign xticklabels
-    hold on
     
     % print mean of each distribution
     text((i - 0.5), 1.03, ...
@@ -229,6 +233,7 @@ legtxt3 = cell(n_shot, 1); % allocate legends for post-flame snapshots
 
 scat3 = cell(n_shot, 1); % allocate scatterplots
 
+hold on
 for i = 1 : n_shot
     
     % generate label for snapshots
@@ -254,7 +259,6 @@ for i = 1 : n_shot
     
     scat3{i} = scatter(parsdata(i).npp, parsdata(i).sagg, ms3(i),...
         clr2(i,:), mt3{i}, 'LineWidth', 1);
-    hold on
 
 end
 
@@ -277,4 +281,87 @@ ylabel('$s_\mathrm{agg}$ [-]', 'interpreter', 'latex',...
 % print legends
 legend(cat(1, scat3{:}), legtxt3, 'interpreter', 'latex',...
     'FontSize', 14, 'Location', 'southeast');
+
+%% plot distributions of observed primary particle diameters %%
+
+% initialize figure
+f4 = figure;
+f4.Position = [150, 150, 500, 500];
+set(f4, 'color', 'white');
+
+% allocate ensemble primary particle diameters observed considering...
+    % ...shielding
+dpps_2d{i} = cell(n_shot,1);
+
+% allocate number of observed primary particle diameters in 2d projections
+n_dpps_2d= zeros(n_shot,1);
+
+% allocate boxplots
+bp4 = cell(n_shot,1);
+
+% calculate and plot ensemble mean of primary particle diameter in 3D
+dpp_ens = geomean(cat(1, parsdata(1).dpp));
+plt4_ens = plot(linspace(0, n_shot+2, 100), 1e9 * repmat(dpp_ens,1,100),...
+    'Color', [0, 0, 0], 'LineWidth', 1.5, 'LineStyle', ':');
+hold on
+
+for i = 1 : n_shot
+
+    % allocate and calculate geometric mean of primary particle...
+    % ...diameter in 2d projections
+    parsdata(i).dpp_2d = zeros(nagg(i),1);
+    for j = 1 : nagg(i)
+        jj = parsdata(i).spp{j} > spp_star;
+        parsdata(i).dpp_2d(j) = geomean(parsdata(i).pp{j}(jj,2));
+    end
+
+    % compile all primary particles acrosss aggregates
+    dpps_2d{i} = cat(1, parsdata(i).dpp_2d);
+
+    % total number of observable primary particles
+    n_dpps_2d = length(dpps_2d{i});
+
+    % plot distribution of ensemble primary particle size
+    bp4{i} = boxplot(1e9 * dpps_2d{i}, 'Positions', i,...
+        'Notch', 'on', 'Symbol', 'o', 'Widths', 0.5);
+    
+    % Find the box object
+    boxObj = findobj(bp4{i}, 'Tag', 'Box');    
+    
+    % fill inside the box
+    patch(get(boxObj, 'XData'), get(boxObj, 'YData'), clr2(i, :),...
+        'FaceAlpha', 0.3, 'EdgeColor', clr2(i, :), 'LineWidth', 1);
+    
+    % adjust the median line
+    boxMed = findobj(bp4{i}, 'Tag', 'Median');
+    set(boxMed, 'Color', clr2(i, :), 'LineWidth', 2);
+    
+    % adjust outlier markers
+    outliers = findobj(bp4{i}, 'Tag', 'Outliers');
+    outliers.MarkerEdgeColor = clr2(i, :);
+    outliers.MarkerSize = 3;
+    
+    % adjust whiskers
+    upwhisker = findobj(bp4{i},'type', 'line', 'tag', 'Upper Whisker');
+    set(upwhisker, 'linestyle', '-');
+    lowwhisker= findobj(bp4{i}, 'type', 'line','tag', 'Lower Whisker');
+    set(lowwhisker, 'linestyle', '-');
+    
+end
+
+xticks(1 : n_shot)  % specify tick positions for horizontal axis
+xticklabels(xlbl2)  % assign labels to ticks
+
+% set plot appearances
+box on
+set(gca, 'TickLabelInterpreter', 'latex', 'FontSize', 12,...
+    'TickLength', [0.02 0.02], 'YScale', 'log')
+xlim([0 n_shot+1])
+xlabel('$n_\mathrm{agg}/(n_\mathrm{agg})_2$ [-]', 'interpreter', 'latex',...
+    'FontSize', 18)
+ylabel('$\overline{d}_\mathrm{pp}^\mathrm{(2D)}$ [nm]', 'interpreter', 'latex',...
+    'FontSize', 18)
+xlim([0.25 n_shot+0.75])
+legend(plt4_ens, '$\langle{d_\mathrm{pp}}\rangle$',...
+    'interpreter', 'latex', 'FontSize', 14, 'location', 'northeast')
 
